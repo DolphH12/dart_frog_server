9b96d2c6f471db52bffb258f0a234a297e356d87
Dolph Hincapie <dolph.hincapie@DOLPH-HINCAPIE.local>
2025-05-04 12:25:55 -0500

    Create bin/server

---
+++ bin/server.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import '../routes/ws.dart' as ws_handler;
+
+void main() async {
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  const handler = ws_handler.onRequest;
+  await serve(handler, ip, port);
+}
